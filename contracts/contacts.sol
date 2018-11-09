16a1c93e88fc2621f05944c90e27229076bdae59
java2man <hattori.zheng@gmail.com>
2018-11-09 15:15:05 +0900

    Update Contact.sol

--- contracts/contacts.sol
+++ contracts/contacts.sol
@@ -1,54 +1,57 @@
-pragma solidity ^ 0.4.25;
+pragma solidity ^0.4.25;
 pragma experimental ABIEncoderV2;
 
 contract Contact {
-
-  struct contact {
-    uint id;
-    string name;
-    string contactAddress;
-    string tel;
-  }
-
-  contact[] contacts;
-  uint id;
-
-  function Contract() {
-    id = 0;
-  }
-
-  function addContact(string name, string contactAddress, string tel) {
-    id += 1;
-    contacts.push(contact(id, name, contactAddress, tel));
-  }
-
-  function updateContact(uint id, string name, string contactAddress, string tel) {
-    for (uint i = 0; i < contacts.length; i++) {
-      if (contacts[i].id == id) {
-        contacts[i].name = name;
-        contacts[i].contactAddress = contactAddress;
-        contacts[i].tel = tel;
-        break;
-      }
+    
+    address owner;
+    uint contractId;
+    contact[] contacts;
+    
+    struct contact {
+        uint id;
+        string name;
+        string contactAddress;
+        string tel;
     }
-  }
-
-  function deleteContact(uint id) {
-    if (contacts.length == 0) {
-      revert("No contact yet!");
+    
+    constructor () public {
+        owner = msg.sender;
+        contractId = 0;
     }
-
-    for (uint i = 0; i < contacts.length; i++) {
-      if (contacts[i].id == id) {
-        delete contacts[i];
-        contacts[i] = contacts[contacts.length - 1];
-        delete contacts[contacts.length - 1];
-      }
+    
+    function addContact (string name, string contactAddress, string tel) public {
+        contractId += 1;
+        contacts.push(contact(contractId, name, contactAddress, tel));
     }
-  }
-
-  function getContactList() constant returns(contact[]) {
-    return contacts;
-  }
-
+    
+    function updateContact (uint id, string name, string contactAddress, string tel) public {
+        if (id > contacts.length) return;
+        
+        for (uint i = 0; i < contacts.length; i++) {
+            if (contacts[i].id == id) {
+                contacts[i].name = name;
+                contacts[i].contactAddress = contactAddress;
+                contacts[i].tel = tel;
+                break;
+            }
+        }
+    }
+    
+    function deleteContact (uint id) public {
+        if (id > contacts.length) return;
+        
+        for (uint i = 0; i < contacts.length; i++) {
+            if (contacts[i].id == id) {
+                delete contacts[i];
+                contacts[i] = contacts[contacts.length - 1];
+                delete contacts[contacts.length - 1];
+                contacts.length--;
+            }
+        }
+    }
+    
+    function getContactList () public constant returns (contact[]) {
+        return contacts;
+    }
+    
 }
